d46251a53a660e7da31afa67193923992df5ebf7
selvendran <selvendranks@gmail.com>
2026-04-16 22:53:19 +0200

    Added water way scene

---
+++ Assets/Bitgem/StylisedWater/URP/Scripts/Bitgem/VFX/StylisedWater/WateverVolumeFloater.cs
@@ -0,0 +1,49 @@
+﻿#region Using statements
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#endregion
+
+namespace Bitgem.VFX.StylisedWater
+{
+    public class WateverVolumeFloater : MonoBehaviour
+    {
+        #region Public fields
+
+        public WaterVolumeHelper WaterVolumeHelper = null;
+
+        #endregion
+
+        #region Serialized fields
+
+        // Visible in Inspector, but private in code
+        [SerializeField] private float heightOffset = 0.5f;
+
+        #endregion
+
+        #region MonoBehaviour events
+
+        void Update()
+        {
+            var instance = WaterVolumeHelper ? WaterVolumeHelper : WaterVolumeHelper.Instance;
+            if (!instance)
+            {
+                return;
+            }
+
+            // Get water height at this position
+            float waterHeight = instance.GetHeight(transform.position) ?? transform.position.y;
+
+            // Apply offset so object floats higher
+            transform.position = new Vector3(
+                transform.position.x,
+                waterHeight + heightOffset,
+                transform.position.z
+            );
+        }
+
+        #endregion
+    }
+}
