1d3188a2acdf8a4ebc9cafe8252ca0104049cb6d
selvendran <selvendranks@gmail.com>
2026-04-22 19:42:07 +0200

    added scene resetter and updated ship movement script

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MoveAlongX : MonoBehaviour
 {
     [SerializeField] private float endX = 40f;
     [SerializeField] private float speed = 2f;
     [SerializeField] private float stopThreshold = 0.001f;
+    [SerializeField] private Key startKey = Key.Space;
 
     public bool HasReachedDestination { get; private set; }
+    public bool IsMoving => isMoving;
+    private bool isMoving = false;
+
+    public void StartMovement()
+    {
+        if (HasReachedDestination) return;
+        isMoving = true;
+    }
 
     void Update()
     {
-        if (HasReachedDestination)
+        if (Keyboard.current != null && Keyboard.current[startKey].wasPressedThisFrame)
+        {
+            StartMovement();
+        }
+
+        if (!isMoving || HasReachedDestination)
             return;
 
         Vector3 targetPosition = new Vector3(endX, transform.position.y, transform.position.z);
@@ -20,6 +35,7 @@ public class MoveAlongX : MonoBehaviour
         {
             transform.position = targetPosition;
             HasReachedDestination = true;
+            isMoving = false;
         }
     }
 }
